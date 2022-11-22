efbac552cd24ea5e6c22730b66026f49006d0a11
roberthjc <roberthjc03@gmail.com>
2022-11-22 12:44:32 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
+//ENTRADA
+const email = document.getElementById("txtEmail");
+const error = document.getElementById("emailvalidacion");
+const btnEnviar=document.getElementById("btnEnviar");
+let mensaje=""; 
+btnEnviar.onclick = function(){
+    let vemail= email.value;
+    let arroba =0;
+   
+    //PROCESO
+     arroba=vemail.indexOf("@");
+    if( arroba < 0 ){
+        mensaje= "Email Invalido"
+    }else{
+        window.location.replace("pagina1.html")        
+        
+
+
+    }
+//  SALIDA
+error.innerText=mensaje;
+
+}
+
+
 
